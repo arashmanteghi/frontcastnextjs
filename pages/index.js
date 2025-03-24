4b99e312dcd266bdb249d7c510bfc83bee5b29f3
Arash Manteghi <arash.manteghi@gmail.com>
2025-03-24 23:05:24 +0100

    12-link-component

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
+import Link from 'next/link';
+
 function HomePage() {
   return (
     <div>
       <h2>Home Page</h2>
+      <ul>
+        <li><Link href="/blog/posts">Blog</Link></li>
+      </ul>
     </div>
   )
 }
