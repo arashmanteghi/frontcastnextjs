16216755ae8e254378e3133128c2e751fe9cc3c6
Arash Manteghi <arash.manteghi@gmail.com>
2025-03-24 23:21:34 +0100

    13 - navigating to dynamic routes

--- pages/users/index.js
+++ pages/users/index.js
@@ -1,7 +1,24 @@
+import Link from 'next/link'
+
 function UsersPage() {
+
+  const users = [
+    { id: 1, username: 'Arash' },
+    { id: 2, username: 'Admin' }
+  ]
+
   return (
     <div>
       <h2>User Page</h2>
+      <ul>
+        {users.map(user => (
+          <li key={user.id}>
+            <Link href={`/users/${user.id}`}>{user.username}</Link>
+          </li>
+        ))}
+        {/* <li><Link href="/users/arash">Arash</Link></li>
+        <li><Link href="/users/admin">Admin</Link></li> */}
+      </ul>
     </div>
   )
 }
